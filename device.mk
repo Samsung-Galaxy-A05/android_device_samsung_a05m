0899c8c75d2050d3797eac5a8c8e06e67c3a8083
drosen-google <drosen-google@gmail.com>
2026-01-15 10:55:26 +0530

    a05m: Enable Project ID Quota support on userdata
     * These features enable wide inodes on userdata, which
       is required for project ID quotas. Wide inodes can
       only be set at MKFS time.
    
     * This also enables casefolding, which is required for the
       removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Signed-off-by: Smiley9000 <horoidrom@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # fastbootd
 PRODUCT_PACKAGES += \
