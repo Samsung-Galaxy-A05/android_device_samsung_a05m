e0c89f389962f15e6796b2cd937ef5c05b1e9e3b
ItsVixano <priv.luk@gmail.com>
2026-06-23 19:35:58 +0530

    a05m: Move to common QTI Vibrator service

--- device.mk
+++ device.mk
@@ -253,8 +253,13 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor 
 
 # Vibrator
+$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-V2-ndk.vendor
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Vndservice manager
 PRODUCT_PACKAGES += \
