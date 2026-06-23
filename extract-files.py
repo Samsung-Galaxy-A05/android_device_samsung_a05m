301a9d6fcfc46b9a6dc1c5bdc92bcc186fe8d23c
Smiley9000 <horoidrom@gmail.com>
2026-06-23 22:42:48 +0530

    a05m: Fix conflicts with vendor blobs

--- extract-files.py
+++ extract-files.py
@@ -41,7 +41,8 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-
+    'libuuid': lib_fixup_vendor_suffix,
+    'libuuid.dylib': lib_fixup_vendor_suffix,
 }
 
 
