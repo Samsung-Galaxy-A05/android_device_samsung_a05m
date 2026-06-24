5d2d734d663bb9803435fea0b89d1440af905c25
bengris32 <bengris32@protonmail.ch>
2026-06-24 22:21:02 +0530

    a05m: Inherit common MediaTek framework compatibility matrix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -233,6 +233,7 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/samsung_framework_compatibility_matrix.xml
 
 # WiFi
