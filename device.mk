59e58f3d9220a0264bd97c2ccc313c701b8ddaaa
bengris32 <bengris32@protonmail.ch>
2026-06-24 22:06:21 +0530

    a05m: Ship AOSP cgroups and task_profiles configs

--- device.mk
+++ device.mk
@@ -53,6 +53,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth-service.mediatek
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapmaxfree=12m \
