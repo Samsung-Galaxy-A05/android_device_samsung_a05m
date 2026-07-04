007de206fd79fd50df5658c1c4b1d5d67d1d4576
ris <rissu.ntk@gmail.com>
2026-07-04 22:47:51 +0700

    a05m: Switch to Mediatek AIDL FCM
    * Fix issue with vintf checker
    
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -244,7 +244,7 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix_aidl.xml \
     $(DEVICE_PATH)/configs/vintf/samsung_framework_compatibility_matrix.xml
 
 # Wi-Fi
