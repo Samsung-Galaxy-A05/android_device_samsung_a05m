5e37f4f95e9aeb1f5493849e0467910c0e8bece5
ris <rissu.ntk@gmail.com>
2026-07-15 21:42:49 +0700

    Revert "a05m: Inherit MSSI common overlays"
    This reverts commit d786c815f5ae04497aad1eeed50d656992475350.
    
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- device.mk
+++ device.mk
@@ -141,14 +141,13 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt   
 
 # Overlays
-$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
-
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 PRODUCT_PACKAGES += \
     FrameworksResOverlay \
     LineagePlatfromOverlay \
     SystemUIResOverlay \
-    NcmTetheringOverlay 
+    NcmTetheringOverlay \
+    WifiOverlay
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
